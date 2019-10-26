351a69c0f28a5be6d9d265790a855639d4e06712
Sascha Majewsky <sascha.majewsky@pm.me>
2019-10-26 15:57:23 +0200

    Parsing values from UI via eventlistener

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
+// DOM Elements
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
+
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+};
+
+generateEl.addEventListener('click', () => {
+  // String to number with + prefix
+  const length = +lengthEl.value;
+  const hasUpper = uppercaseEl.checked;
+  const hasLower = lowercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbol = symbolsEl.checked;
+
+  console.log(length, hasLower, hasUpper, hasNumber, hasSymbol);
+});
+
 // Generator functions
 // See chars at http://net-comber.com/charset.html String.fromCharCode(97)
 
